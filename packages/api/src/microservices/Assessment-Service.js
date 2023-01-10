b700cf5e045a043cf8c6798238af0a5a19b3952c
Jamernacht99 <lamjs@mail.uc.edu>
2023-01-10 13:17:23 -0500

    view assessments list

--- packages/api/src/microservices/Assessment-Service.js
+++ packages/api/src/microservices/Assessment-Service.js
@@ -5,21 +5,21 @@ exports.submit = async (assessment) => {
   // use the sequelize model Assessments from packages/api/src/database/models to save
   // the assessment data in the PostgreSQL database
 
-  // console.log(`assessment`, assessment);
-
   await Assessment.create({
     catDateOfBirth: assessment.catDateOfBirth,
     catName: assessment.catName,
+    instrumentID: assessment.instrumentID,
     instrumentType: assessment.instrumentType,
     riskLevel: assessment.riskLevel,
     score: assessment.score,
   });
 };
 
-exports.getList = () => {
+exports.getList = async () => {
   // use the sequelize model Assessments from packages/api/src/database/models to fetch
   // the assessment data from the PostgreSQL database
-  const assessments = [];
+  const assessments = await Assessment.findAll();
+  // console.log(assessments);
 
   return assessments;
 };
