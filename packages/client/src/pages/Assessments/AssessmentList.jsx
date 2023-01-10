5490784ddd04e01df1c05c4839615540718f5e11
Jamernacht99 <lamjs@mail.uc.edu>
2023-01-10 13:56:44 -0500

    fixes

--- packages/client/src/pages/Assessments/AssessmentList.jsx
+++ packages/client/src/pages/Assessments/AssessmentList.jsx
@@ -33,61 +33,59 @@ function Table({ columns, data }) {
   // Render the UI for your table
   return (
     <>
-      <div style={{ display: `flex`, flexDirection: `column` }}>
-        <div>
-          <table {...getTableProps()}>
-            <thead>
-              {headerGroups.map((headerGroup) => <tr {...headerGroup.getHeaderGroupProps()}>
-                {headerGroup.headers.map((column) => <th {...column.getHeaderProps()}>{column.render(`Header`)}</th>)}
-              </tr>)}
-            </thead>
-            <tbody {...getTableBodyProps()}>
-              {page.map((row) => {
-                prepareRow(row);
-                return (
-                  <tr {...row.getRowProps()}>
-                    {row.cells.map((cell) => <td {...cell.getCellProps()}>{cell.render(`Cell`)}</td>)}
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
-        </div>
-        <br />
-        <div>
-          <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
-            {`<<`}
-          </button>{` `}
-          <button onClick={() => previousPage()} disabled={!canPreviousPage}>
-            Previous
-          </button>{` `}
-          <button onClick={() => nextPage()} disabled={!canNextPage}>
-            Next
-          </button>{` `}
-          <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
-            {`>>`}
-          </button>{` `}
-          <span>
-            Page{` `}
-            <strong>
-              {pageIndex + 1} of {pageOptions.length}
-            </strong>{` `}
-          </span>
-          <span>
-            | Go to page:{` `}
-            <input
-              type="number"
-              defaultValue={pageIndex + 1}
-              onChange={(e) => {
-                const pageNumber = e.target.value ?
-                  Number(e.target.value) - 1 :
-                  0;
-                gotoPage(pageNumber);
-              }}
-              style={{ width: `50px` }} />
-          </span>{` `}
+      <div>
+        <table {...getTableProps()}>
+          <thead>
+            {headerGroups.map((headerGroup) => <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column) => <th {...column.getHeaderProps()}>{column.render(`Header`)}</th>)}
+            </tr>)}
+          </thead>
+          <tbody {...getTableBodyProps()}>
+            {page.map((row) => {
+              prepareRow(row);
+              return (
+                <tr {...row.getRowProps()}>
+                  {row.cells.map((cell) => <td {...cell.getCellProps()}>{cell.render(`Cell`)}</td>)}
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+      <br />
+      <div>
+        <button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
+          {`<<`}
+        </button>{` `}
+        <button onClick={() => previousPage()} disabled={!canPreviousPage}>
+          Previous
+        </button>{` `}
+        <button onClick={() => nextPage()} disabled={!canNextPage}>
+          Next
+        </button>{` `}
+        <button onClick={() => gotoPage(pageCount - 1)} disabled={!canNextPage}>
+          {`>>`}
+        </button>{` `}
+        <span>
+          Page{` `}
+          <strong>
+            {pageIndex + 1} of {pageOptions.length}
+          </strong>{` `}
+        </span>
+        <span>
+          | Go to page:{` `}
+          <input
+            type="number"
+            defaultValue={pageIndex + 1}
+            onChange={(e) => {
+              const pageNumber = e.target.value ?
+                Number(e.target.value) - 1 :
+                0;
+              gotoPage(pageNumber);
+            }}
+            style={{ width: `50px` }} />
+        </span>{` `}
 
-        </div>
       </div>
 
     </>
@@ -138,11 +136,12 @@ export const AssessmentList = () => {
   );
 
   return (
-    <div>
+    <div >
       <h1>Assessment List</h1>
       <hr />
-      <Table columns={columns} data={assessments} />
-
+      <div className="tableContainer">
+        <Table columns={columns} data={assessments} />
+      </div>
     </div>
   );
 };
